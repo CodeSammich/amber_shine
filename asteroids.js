ab0832e01e09ce09eda713229f8eb535293f57ae
ivan <ivlin112@gmail.com>
2016-03-06 12:20:07 -0500

    Still needs much work. We should make another class for player when we start making asteroids

--- asteroids.js
+++ asteroids.js
@@ -1,27 +1,32 @@
 /*--
   Constructor for an object in asteroids
 --*/
-var AsteroidsObject = function AsteroidsObject(){
-    this.angle = this.xcor = this.ycor = this.xvel = this.yvel = this.accel = 0;
+var AsteroidsObject = function(){
+    this.angle = this.xcor = this.ycor = this.vel = this.accel = 0;
     this.draw = function(){
 	ctx.strokeRect(this.xcor,this.ycor,10,20);
     }
     this.update = function(){
-	if (this.accel > 0 ){
-	    if (this.xvel < 1 && this.xvel > -1){
-		this.xvel+=(this.accel*Math.cos(this.angle))%2.5;
+	if (this.accel > 0){
+	    var newvel = this.vel + this.accel;
+	    if (newvel > -4 && newvel < 4){ //stay below maximum velocity
+		this.vel = newvel;
 	    }
-	    if (this.yvel < 1 && this.yvel > -1){
-		this.yvel+=(this.accel*Math.sin(this.angle))%2.5;
-	    }
-	    console.log(this.yvel);
 	}
-	if (this.xcor >= 0 && this.xcor < canvas.width){
-	    this.xcor+=this.xvel;
+	else{ //natural deceleration
+	    if (this.vel > 0)
+		this.vel-=0.01;
+	    if (this.vel < 0)
+		this.vel+=0.01;
 	}
-	if (this.ycor >= 0 && this.ycor < canvas.height){
-	    this.ycor+=this.yvel;
+	if (leftPress){
+	    this.angle = (this.angle-0.05)%(2*Math.PI);
 	}
+	if (rightPress){
+	    this.angle = (this.angle+0.05)%(2*Math.PI);
+	}
+	this.xcor+=this.vel*Math.cos(this.angle);
+	this.ycor+=this.vel*Math.sin(this.angle);
 	this.draw();
     }
 };
@@ -32,8 +37,8 @@ var AsteroidsObject = function AsteroidsObject(){
 
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-var upPress, downPress, leftPress, rightPress;
 var player;
+var leftPress, rightPress;
 
 ctx.fillStyle = "#000";
 ctx.strokeStyle= "#FFF";
@@ -42,16 +47,25 @@ ctx.fillRect(0,0,canvas.width,canvas.height);
 var setupKeypress = function setupKeypress(){
     document.addEventListener("keydown",function(e){
 	if (e.keyCode == 38){ //up
-	    player.accel=1;
+	    player.accel=0.1;
 	}
 	else if (e.keyCode == 37){//left
-	    player.angle = (player.angle+0.01)%(2*Math.PI);
+	    leftPress = true;
 	}
 	else if (e.keyCode == 39){//right
-	    player.angle = (player.angle-0.01)%(2*Math.PI);
+	    rightPress = true;
 	}
     });
     document.addEventListener("keyup",function(e){
+	if (e.keyCode == 38){
+	    player.accel=0;
+	}
+	else if (e.keyCode == 37){//left
+	    leftPress = false;
+	}
+	else if (e.keyCode == 39){//right
+	    rightPress = false;
+	}
     });
 };
 
@@ -63,6 +77,8 @@ var drawCanvas = function drawCanvas(){
 
 var setup = function setup(){
     player = new AsteroidsObject();
+    player.xcor = canvas.width/2;
+    player.ycor = canvas.height/2;
     setupKeypress();
     drawCanvas();
 };
